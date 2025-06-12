23b3c72a9e50db71c389489d1e96c5afd76b03b8
Hung <hungntse182394@fpt.edu.vn>
2025-06-12 10:40:35 +0700

    Revert "google login"
    This reverts commit 40a0bc8b0f894cd8a53cfe9060da9ed12ce85337.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ dependencies {
     implementation(libs.activity)
     implementation(libs.constraintlayout)
     implementation(libs.firebase.auth)
-    implementation(libs.play.services.auth)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
