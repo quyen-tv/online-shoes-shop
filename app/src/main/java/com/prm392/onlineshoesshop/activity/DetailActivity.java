6898606964c4e59cd796af63746c35722b7d0c7d
quyen-tv <tvq05112003@gmail.com>
2025-06-21 14:43:22 +0700

    Completed item detail UI

---
+++ app/src/main/java/com/prm392/onlineshoesshop/activity/DetailActivity.java
@@ -0,0 +1,20 @@
+package com.prm392.onlineshoesshop.activity;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.prm392.onlineshoesshop.R;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
